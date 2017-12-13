1366eb820a50583550b0d7d5c110d2db775d792b
jgurowsky@gmail.com <jgurowsky@gmail.com>
2017-12-13 04:58:40 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,8 +5,8 @@ describe "age" do
     current_year = Time.now.year
     birth_year = 1990
     answer = current_year - birth_year
-    
+
     age_of_person = current_age_for_birth_year(birth_year)
-    expect(age_of_person).to eq(answer) 
+    expect(age_of_person).to eq(answer)
   end
 end
